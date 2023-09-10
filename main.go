efcface2aeee5d61e346cce6463c7309442aaa48
Genaro-Chris <christian25589@gmail.com>
2023-09-10 23:09:07 +0100

    Initial Commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Result struct {
+	Slack_name      string `json:"slack_name"`
+	Current_day     string `json:"current_day"`
+	Utc_time        string `json:"utc_time"`
+	Track           string `json:"track"`
+	Github_file_url string `json:"github_file_url"`
+	Github_repo_url string `json:"github_repo_url"`
+	Status_code     string `json:"status_code"`
+}
+
+func new_result(slack_name string, track string) Result {
+	return Result{
+		Slack_name:      slack_name,
+		Current_day:     time.Now().Weekday().String(),
+		Utc_time:        time.Now().UTC().Format("2006-01-02T15:04:05.000Z"),
+		Track:           track,
+		Github_file_url: "jhjh",
+		Github_repo_url: "jhgvjgv",
+		Status_code:     "200",
+	}
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+
+	slack_name, track := r.URL.Query().Get("slack_name"), r.URL.Query().Get("track")
+	result := new_result(slack_name, track)
+	res, err := json.Marshal(result)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func main() {
+	http.HandleFunc("/", handle)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
